65998735b99647e0ce7ba1648834dca457253b11
Abdelhady "H2O" S <hadysalah1455@gmail.com>
2021-08-19 04:54:12 +0200

    Initialize project
    - Add file architecture
    - Add copyright notice
    - Update README.md with more details about the project

--- public_html/css/style.css
+++ public_html/css/style.css
@@ -1,31 +1,3 @@
-# node-weather-journal
-A weather journal application project for Udacity
-
-Notice: API credentials included in this repository may not be used in production.
-
-## Architecture
-- public_html
-    - css
-        - style.css (landing page styling)
-    - js
-        - app.js (client-side javascript code)
-    - index.html (landing page for visitors, includes all client-side code and the stylesheet)
-- .gitignore (uses the Node gitignore default template from GitHub)
-- LICENSE (GNU GPL v3)
-- README.md (this file, includes some details about the project)
-- server.js (server-side javascript code, uses the Node runtime)
-
-## Dependencies
-- Node JavaScript runtime environment
-
-## Features
-- To be added
-
-## Known Bugs
-- To be added
-
-## Notes
-```js
 /*
  *     AUTHOR: 
  *     -   Abdelhady 'H2O' Salah
@@ -45,4 +17,3 @@ Notice: API credentials included in this repository may not be used in productio
  * 
  *     -   You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
  */
-```
